ace5176d76d6f84a343c3d553b669dd5538d7cb9
meloman2798 <tratill2798@gmail.com>
2020-04-23 15:06:48 +0300

    добавил файлы добовления, инфо

--- create.php
+++ create.php
@@ -5,16 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-<div class="city">
-    <p><label>Kiev</label> <a href="info.php">Info</a>
-        <a href="#">Delete</a> </p>
-    <p><label>Kharkov</label> <a href="info.php">Info</a>
-        <a href="#">Delete</a></p>
-</div>
-<a href="create.php">Add</a>
+<form action="index.php" method="post">
+    <p>Назваеие города:</p>
+    <input type="text">
+    <p><input type="file" name="photo" multiple accept="image/*,image/jpeg">
+        <button> save</button>
+</form>
 </body>
 </html>
--- info.php
+++ info.php
@@ -5,16 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-<div class="city">
-    <p><label>Kiev</label> <a href="info.php">Info</a>
-        <a href="#">Delete</a> </p>
-    <p><label>Kharkov</label> <a href="info.php">Info</a>
-        <a href="#">Delete</a></p>
-</div>
-<a href="create.php">Add</a>
+<p>Название города:<?php $_POST['']?></p>
+<p>Описание:</p>
+
 </body>
 </html>
